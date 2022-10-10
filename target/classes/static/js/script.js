29d00ffc6e84304e8e378a338165e88a11e3a293
Niklas <nineax007mail.com>
2022-10-10 21:51:39 +0200

    Frontend eingebaut

---
+++ target/classes/static/js/script.js
@@ -0,0 +1,69 @@
+var turns = [["#", "#", "#"], ["#", "#", "#"], ["#", "#", "#"]];
+var turn = "";
+var gameOn = false;
+
+function playerTurn(turn, id) {
+    if (gameOn) {
+        var spotTaken = $("#" + id).text();
+        if (spotTaken === "#") {
+            makeAMove(playerType, id.split("_")[0], id.split("_")[1]);
+        }
+    }
+}
+
+function makeAMove(type, xCoordinate, yCoordinate) {
+    $.ajax({
+        url: url + "/games/tic-tac-toe/turn",
+        type: 'POST',
+        dataType: "json",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "gameID": gameID,
+            "type": type,
+            "target": {
+            "row": xCoordinate,
+            "column": yCoordinate
+            }
+        }),
+        success: function (data) {
+            gameOn = false;
+            displayResponse(data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    })
+}
+
+function displayResponse(data) {
+    let board = data.board;
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (String(board[i][j]) === "player1") {
+                turns[i][j] = 'X'
+            } else if (String(board[i][j]) === "player2") {
+                turns[i][j] = 'O';
+            }
+            let id = i + "_" + j;
+            $("#" + id).text(turns[i][j]);
+        }
+    }
+    if (data.winner != null) {
+        alert("Winner is " + data.winner);
+    }
+    gameOn = true;
+}
+
+$(".tic").click(function () {
+    var slot = $(this).attr('id');
+    playerTurn(turn, slot);
+});
+
+function reset() {
+    turns = [["#", "#", "#"], ["#", "#", "#"], ["#", "#", "#"]];
+    $(".tic").text("#");
+}
+
+$("#reset").click(function () {
+    reset();
+});
---
+++ target/classes/static/js/socket_js.js
@@ -0,0 +1,109 @@
+const url = 'http://localhost:8080';
+let stompClient;
+let gameID;
+let playerType;
+
+function connectToSocket(gameID) {
+
+    console.log("connecting to the game");
+    let socket = new SockJS(url + "/gameplay-tic-tac-toe");
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log("connected to the frame: " + frame);
+        stompClient.subscribe("/topic/tic-tac-toe-game-progress/" + gameID, function (response) {
+            let data = JSON.parse(response.body);
+            console.log(data);
+            displayResponse(data);
+        })
+    })
+}
+
+function create_game() {
+    let playerName = document.getElementById("playerName").value;
+    if (playerName == null || playerName === '') {
+        alert("Please enter playerName");
+    } else {
+        $.ajax({
+            url: url + "/games/tic-tac-toe/create",
+            type: 'POST',
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "name": playerName
+            }),
+            success: function (data) {
+                gameID = data.gameID;
+                playerType = 'X';
+                reset();
+                connectToSocket(gameID);
+                alert("Your created a game. Game id is: " + data.gameID);
+                gameOn = true;
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+}
+
+
+function connectToRandom() {
+    let playerName = document.getElementById("playerName").value;
+    if (playerName == null || playerName === '') {
+        alert("Please enter playerName");
+    } else {
+        $.ajax({
+            url: url + "/games/tic-tac-toe/random",
+            type: 'POST',
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "name": playerName
+            }),
+            success: function (data) {
+                gameID = data.gameID;
+                playerType = 'O';
+                reset();
+                connectToSocket(gameID);
+                alert("Congrats you're playing with: " + data.player1.name);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+}
+
+function connectToSpecificGame() {
+    let playerName = document.getElementById("playerName").value;
+    if (playerName == null || playerName === '') {
+        alert("Please enter playerName");
+    } else {
+        let gameID = document.getElementById("game_id").value;
+        if (gameID == null || gameID === '') {
+            alert("Please enter game id");
+        }
+        $.ajax({
+            url: url + "/games/tic-tac-toe/connect",
+            type: 'POST',
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "player": {
+                    "name": playerName
+                },
+                "gameID": gameID
+            }),
+            success: function (data) {
+                gameID = data.gameID;
+                playerType = 'O';
+                reset();
+                connectToSocket(gameID);
+                alert("Congrats you're playing with: " + data.player1.name);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        })
+    }
+}
