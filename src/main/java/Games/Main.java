c6f4c6f877225078efedb57e36c036a4190f71cb
Niklas <nineax007mail.com>
2022-10-25 20:12:09 +0200

    TODOs

--- src/main/java/Games/Main.java
+++ src/main/java/Games/Main.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
     //@TODO Homepage einbauen, bei der man die Spiele auswählen kann (homepage -> page tic tac toe
 
-    //@TODO Refector
+    //TODO Anwendung aus dem Internet aufrufbar machen
+
+    //@TODO GameIdeen: 4-Gewinnt, Schiff-versenken, Mensch-Ärger-Dich-Nicht, Dame, Sudoku(generator + solver), Galgenmännchen, Errate meine Zahl
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
